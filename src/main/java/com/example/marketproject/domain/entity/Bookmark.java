be2c7d95e95cc2adca3df751f608765ff241bf98
wwnss00 <wns208500@gmail.com>
2025-12-11 17:51:00 +0900

    bookmark 추가

--- src/main/java/com/example/marketproject/domain/entity/Bookmark.java
+++ src/main/java/com/example/marketproject/domain/entity/Bookmark.java
@@ -1,2 +1,33 @@
-package com.example.marketproject.domain.entity;public class Bookmark {
+package com.example.marketproject.domain.entity;
+
+import com.example.marketproject.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bookmarks",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_user_post",
+                        columnNames = {"user_id", "post_id"}
+                )
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
