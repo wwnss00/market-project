692a004c6c42bac7c626977d412ab2592847e56f
wwnss00 <wns208500@gmail.com>
2026-01-26 18:30:30 +0900

    feat:PostNotFoundException예외처리 클래스 생성

---
+++ src/main/java/com/example/marketproject/exception/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.marketproject.exception;
+
+public class PostNotFoundException extends RuntimeException{
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
